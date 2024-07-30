9a2cf74e6cae6de9c5ec39f28282261d39ce56f4
Piyawish Piyawat <piyawishone@gmail.com>
2024-07-31 00:19:36 +0700

    refactor py2pi to smaller functions

--- src/piyathon/py2pi.py
+++ src/piyathon/py2pi.py
@@ -1,44 +1,40 @@
 import sys
 import os
-import ast
 import tokenize
 from io import StringIO
 from keywords import PYTHON_TO_THAI
 
-KEYWORD_NODE_TYPES = (
-    ast.FunctionDef,
-    ast.AsyncFunctionDef,
-    ast.ClassDef,
-    ast.For,
-    ast.AsyncFor,
-    ast.While,
-    ast.If,
-    ast.With,
-    ast.AsyncWith,
-    ast.Try,
-    ast.ExceptHandler,
-    ast.Return,
-    ast.Yield,
-    ast.YieldFrom,
-    ast.Delete,
-    ast.Assign,
-    ast.AugAssign,
-    ast.Import,
-    ast.ImportFrom,
-    ast.Global,
-    ast.Nonlocal,
-    ast.Assert,
-    ast.Break,
-    ast.Continue,
-    ast.Pass,
-    ast.Raise,
-    ast.Lambda,
-    ast.Await,
-    ast.ListComp,
-    ast.SetComp,
-    ast.DictComp,
-    ast.GeneratorExp,
-)
+
+def handle_from_import(tokens, i, result):
+    result.append(tokens[i]._replace(string=PYTHON_TO_THAI["from"]))
+    i += 1
+    while i < len(tokens) and tokens[i].string != "import":
+        result.append(tokens[i])
+        i += 1
+    if i < len(tokens) and tokens[i].string == "import":
+        result.append(tokens[i]._replace(string=PYTHON_TO_THAI["import"]))
+    return i
+
+
+def handle_else_elif(tokens, i, result):
+    next_token = tokens[i + 1] if i + 1 < len(tokens) else None
+    if next_token and next_token.type == tokenize.NAME and next_token.string == "if":
+        result.append(tokens[i]._replace(string=PYTHON_TO_THAI["elif"]))
+        i += 1  # Skip the 'if' in 'elif'
+    else:
+        result.append(tokens[i]._replace(string=PYTHON_TO_THAI["else"]))
+    return i
+
+
+def handle_for_in(tokens, i, result):
+    result.append(tokens[i]._replace(string=PYTHON_TO_THAI["for"]))
+    i += 1
+    while i < len(tokens) and tokens[i].string != "in":
+        result.append(tokens[i])
+        i += 1
+    if i < len(tokens) and tokens[i].string == "in":
+        result.append(tokens[i]._replace(string=PYTHON_TO_THAI["in"]))
+    return i
 
 
 def transform_to_thai(code):
@@ -49,26 +45,11 @@ def transform_to_thai(code):
         tok = tokens[i]
         if tok.type == tokenize.NAME and tok.string in PYTHON_TO_THAI:
             if tok.string == "from":
-                # Handle 'from ... import ...'
-                result.append(tok._replace(string=PYTHON_TO_THAI["from"]))
-                i += 1
-                while i < len(tokens) and tokens[i].string != "import":
-                    result.append(tokens[i])
-                    i += 1
-                if i < len(tokens) and tokens[i].string == "import":
-                    result.append(tokens[i]._replace(string=PYTHON_TO_THAI["import"]))
+                i = handle_from_import(tokens, i, result)
             elif tok.string == "else":
-                # Check if it's 'else' or 'elif'
-                next_token = tokens[i + 1] if i + 1 < len(tokens) else None
-                if (
-                    next_token
-                    and next_token.type == tokenize.NAME
-                    and next_token.string == "if"
-                ):
-                    result.append(tok._replace(string=PYTHON_TO_THAI["elif"]))
-                    i += 1  # Skip the 'if' in 'elif'
-                else:
-                    result.append(tok._replace(string=PYTHON_TO_THAI["else"]))
+                i = handle_else_elif(tokens, i, result)
+            elif tok.string == "for":
+                i = handle_for_in(tokens, i, result)
             else:
                 result.append(tok._replace(string=PYTHON_TO_THAI[tok.string]))
         else:
@@ -84,11 +65,9 @@ def main():
         sys.exit(1)
 
     input_file = sys.argv[1]
-
-    if len(sys.argv) == 3:
-        output_file = sys.argv[2]
-    else:
-        output_file = os.path.splitext(input_file)[0] + ".pi"
+    output_file = (
+        sys.argv[2] if len(sys.argv) == 3 else os.path.splitext(input_file)[0] + ".pi"
+    )
 
     try:
         with open(input_file, "r", encoding="utf-8") as file:
