1ee7fa12981804d28e47cd3f4e898a9dce99570c
Piyawish Piyawat <piyawishone@gmail.com>
2024-07-31 12:12:02 +0700

    add more Thai translations

--- src/piyathon/py2pi.py
+++ src/piyathon/py2pi.py
@@ -1,39 +1,49 @@
 import sys
 import os
+import ast
 import tokenize
 from io import StringIO
-from keywords import PYTHON_TO_THAI
+from keywords import PY_TO_THAI
+
+
+def check_syntax(code):
+    try:
+        ast.parse(code)
+        return True
+    except SyntaxError as e:
+        print(f"Syntax error in the input file: {e}")
+        return False
 
 
 def handle_from_import(tokens, i, result):
-    result.append(tokens[i]._replace(string=PYTHON_TO_THAI["from"]))
+    result.append(tokens[i]._replace(string=PY_TO_THAI["from"]))
     i += 1
     while i < len(tokens) and tokens[i].string != "import":
         result.append(tokens[i])
         i += 1
     if i < len(tokens) and tokens[i].string == "import":
-        result.append(tokens[i]._replace(string=PYTHON_TO_THAI["import"]))
+        result.append(tokens[i]._replace(string=PY_TO_THAI["import"]))
     return i
 
 
 def handle_else_elif(tokens, i, result):
     next_token = tokens[i + 1] if i + 1 < len(tokens) else None
     if next_token and next_token.type == tokenize.NAME and next_token.string == "if":
-        result.append(tokens[i]._replace(string=PYTHON_TO_THAI["elif"]))
+        result.append(tokens[i]._replace(string=PY_TO_THAI["elif"]))
         i += 1  # Skip the 'if' in 'elif'
     else:
-        result.append(tokens[i]._replace(string=PYTHON_TO_THAI["else"]))
+        result.append(tokens[i]._replace(string=PY_TO_THAI["else"]))
     return i
 
 
 def handle_for_in(tokens, i, result):
-    result.append(tokens[i]._replace(string=PYTHON_TO_THAI["for"]))
+    result.append(tokens[i]._replace(string=PY_TO_THAI["for"]))
     i += 1
     while i < len(tokens) and tokens[i].string != "in":
         result.append(tokens[i])
         i += 1
     if i < len(tokens) and tokens[i].string == "in":
-        result.append(tokens[i]._replace(string=PYTHON_TO_THAI["in"]))
+        result.append(tokens[i]._replace(string=PY_TO_THAI["in"]))
     return i
 
 
@@ -43,7 +53,7 @@ def transform_to_thai(code):
     i = 0
     while i < len(tokens):
         tok = tokens[i]
-        if tok.type == tokenize.NAME and tok.string in PYTHON_TO_THAI:
+        if tok.type == tokenize.NAME and tok.string in PY_TO_THAI:
             if tok.string == "from":
                 i = handle_from_import(tokens, i, result)
             elif tok.string == "else":
@@ -51,7 +61,7 @@ def transform_to_thai(code):
             elif tok.string == "for":
                 i = handle_for_in(tokens, i, result)
             else:
-                result.append(tok._replace(string=PYTHON_TO_THAI[tok.string]))
+                result.append(tok._replace(string=PY_TO_THAI[tok.string]))
         else:
             result.append(tok)
         i += 1
@@ -77,6 +87,10 @@ def main():
         sys.exit(1)
     except IOError:
         print(f"Error: Unable to read input file '{input_file}'.")
+        sys.exit(1)
+
+    if not check_syntax(python_code):
+        print("Translation aborted due to syntax errors in the input file.")
         sys.exit(1)
 
     piyathon_code = transform_to_thai(python_code)
