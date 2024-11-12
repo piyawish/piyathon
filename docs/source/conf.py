20b653a2484bbd2e020f511665438f3a145d4fa8
Piyawish Piyawat <piyawishone@gmail.com>
2024-11-12 22:32:25 +0700

    finished the structure of the site

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,35 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Piyathon Documentation'
-copyright = '2024, Piyawish Piyawat'
-author = 'Piyawish Piyawat'
-release = '0.0.1'
+project = "Piyathon Documentation"
+copyright = "2024, Piyawish Piyawat"  # pylint: disable=redefined-builtin
+author = "Piyawish Piyawat"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-language = 'th'
+language = "th"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "furo"
+html_logo = "_static/logo.png"
+html_title = "Piyathon Language (ภาษาปิยะทอน)"
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
+html_show_sphinx = False
+html_show_sourcelink = False
+html_copy_source = False
+
+# Add these lines to configure theme options
+html_theme_options = {
+    "dark_mode_theme": None,
+    "sidebar_hide_name": True,
+}
